2462e40a2e427902ba54dd6d855c6de39034ff4d
Erwann LELETTY <erwann.le-letty@epitech.eu>
2025-03-11 11:45:57 +0000

    Initial commit

---
+++ tests/test_minesweeper.py
@@ -0,0 +1,20 @@
+# tests/test_minesweeper.py
+
+import pytest
+import minesweeper
+import random
+
+def test_module_exists():
+    assert minesweeper
+
+def test_place_mines():
+    game = minesweeper.Minesweeper(3, 3, 2)
+    game.place_mines()
+    assert len(game.mines) == 2
+
+def test_reveal():
+    random.seed(0)
+    game = minesweeper.Minesweeper(3, 3, 2)
+    game.place_mines()
+    game.reveal(2, 2)
+    assert game.revealed == {(2, 2)}
